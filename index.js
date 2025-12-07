aee6653fb74886c022abf9e1425f838435fcdcf7
shivam kushwah <97247055+Shivamkus@users.noreply.github.com>
2025-12-07 23:27:45 +0530

    add cors origin

--- index.js
+++ index.js
@@ -2,39 +2,30 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
-const cors = require('cors')
-const ConnectDB = require('./config/db')
-const router = require('./router/index')
+const cors = require("cors");
+const ConnectDB = require("./config/db");
+const router = require("./router/index");
 
+app.use(
+  cors({
+    // origin: "https://tasklist-beige.vercel.app/",
+    // origin: "*",
 
-app.use(cors({
-  // origin: "https://tasklist-beige.vercel.app/",
-    origin: "*",
-
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true,
-}));
+    origin: ["https://tasklist-beige.vercel.app/", "http://localhost:5173"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-const appVersion = process.env.APP_VERSION
-app.use(`/api/${appVersion}`, router)
+app.use(`/api/v1`, router);
 
+ConnectDB();
 
-
-
-ConnectDB()
-
-
-
-
-
-
-app.listen(PORT, (err)=> {
-if(err) {
-    console.log(`Error on Running Server`, err)
-}else{
-    console.log(`Server is Running On Port: ${PORT}`)
-}
-})
-
-
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Error on Running Server`, err);
+  } else {
+    console.log(`Server is Running On Port: ${PORT}`);
+  }
+});
